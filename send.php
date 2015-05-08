fdc832df1bf5982d4a38f3731350a6fd073f50c1
Timur Munykin <crazytimon88@gmail.com>
2015-05-08 13:14:33 +0300

    changesendmail

--- send.php
+++ send.php
@@ -4,7 +4,7 @@ $fromemail = 'From: fromemail@frommail.com';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-if(mail($toemail, 'Получена заявка от '.$name, $message.'. Телефон: '.$phone, $fromemail)) {
+if(mail($toemail, 'Получена заявка от '.$name, 'Получена заявка от '.$name.'. '.$message.'. Телефон: '.$phone, $fromemail)) {
 	echo 'Ваше сообщение успешно отправлено.';
 } else {
 	echo 'Проблемы с отправкой.';
